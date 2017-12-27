d4539050fc51f92a0411094aa62b11f215b70719
PallabeeDey <pallabeedey@gmail.com>
2017-12-27 17:29:24 +0530

    first commit

--- ping-api-etherdelta-com/output_file/ping-api-etherdelta-com.py
+++ ping-api-etherdelta-com/output_file/ping-api-etherdelta-com.py
@@ -5,10 +5,12 @@ data=data.rename(columns={'56(84)':'icmp_seq','of':'time'})
 data['time']=data['time'].str.replace('time=', '')
 data['icmp_seq']=data['icmp_seq'].str.replace('icmp_seq=', '')
 data=data.set_index('icmp_seq')
-data['mean_period5']=data['time'].rolling(window=5).mean()
-data['mean_period10']=data['time'].rolling(window=10).mean()
-data['mean_period20']=data['time'].rolling(window=20).mean()
-data['StdDev_period5']=data['time'].rolling(window=5).std()
-data['StdDev_period10']=data['time'].rolling(window=10).std()
-data['StdDev_period20']=data['time'].rolling(window=20).std()
+col1=['mean_period5','mean_period10','mean_period20']
+col2=['StdDev_period5','StdDev_period10','StdDev_period20']
+period = [5,10,20]
+for i,j in zip(col1,period):
+    data[i]=data['time'].rolling(window=j).mean()
+for i,j in zip(col2,period):
+    data[i]=data['time'].rolling(window=j).std()
+data.to_csv('output_ping-api-etherdelta-com.csv')
 
