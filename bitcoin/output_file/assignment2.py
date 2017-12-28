7803f4f87f6c50d0e44a736b3f85a282543d3f6d
PallabeeDey <pallabeedey@gmail.com>
2017-12-28 11:20:10 +0530

    first commit

--- bitcoin/output_file/assignment2.py
+++ bitcoin/output_file/assignment2.py
@@ -12,14 +12,14 @@ bitcoin['Volume (in %)']=pd.to_numeric(bitcoin['Volume (in %)'])
 bitcoin['traded_quantity']=bitcoin['Volume (24h)(in $)']/bitcoin['Price(in $)']
 a=bitcoin.groupby('Source').agg({'Volume (in %)':"sum"})
 a1=a.sort_values(by='Volume (in %)', ascending=0)
-Ans1=a1.iloc[1:20,:]
+Ans1=a1.iloc[0:20,:]
 Ans1.to_csv('top_20_largest_exchange.csv')
 b=bitcoin.groupby('Pair').agg({'traded_quantity':"mean"})
 b1=b.sort_values(by='traded_quantity', ascending=0)
-Ans2=b1.iloc[1:25,:]
+Ans2=b1.iloc[0:25,:]
 Ans2.to_csv('top_25_most_traded_pairs.csv')
 c=bitcoin.groupby('Pair').agg({'Price(in $)':"mean"})
 c1=c.sort_values(by='Price(in $)', ascending=1)
-Ans3=c1.iloc[1:10,:]
+Ans3=c1.iloc[0:10,:]
 Ans3.to_csv('top_10_cheapest_pairs.csv')
 
