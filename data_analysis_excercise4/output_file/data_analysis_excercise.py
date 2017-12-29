92e53d8aee4f0f7f3e82b377178353ce3361b46f
PallabeeDey <pallabeedey@gmail.com>
2017-12-29 17:14:27 +0530

    first commit

--- data_analysis_excercise4/output_file/data_analysis_excercise.py
+++ data_analysis_excercise4/output_file/data_analysis_excercise.py
@@ -2,4 +2,5 @@ data4=pd.read_csv('/home/pallabeedey/Downloads/anom_order.out',sep='_',header=No
 data4['date']=data4['date'].str.replace('.csv', '')
 data4=data4[['source','date']]
 data4['date']=pd.to_datetime(data4['date'], unit='ns')
-data4
+data4_unique_source=pd.DataFrame(data4.source.value_counts())
+data4_unique_source
