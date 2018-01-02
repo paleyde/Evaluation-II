a16f4300afa68e09510ac2475f1242716e4d0311
PallabeeDey <pallabeedey@gmail.com>
2018-01-02 11:01:37 +0530

    first commit

--- data_analysis_excercise4/output_file/data_analysis_excercise.py
+++ data_analysis_excercise4/output_file/data_analysis_excercise.py
@@ -4,10 +4,10 @@ data4['date']=data4['date'].str.replace('.csv', '')
 data4=data4[['coin','date']]
 data4['date']=pd.to_datetime(data4['date'], unit='ns')
 data4_unique_source=pd.DataFrame(data4.coin.value_counts())
-data4_unique_source=data4_unique_source.rename(columns={'coin':'No._of_unique_coin'})
+data4_unique_source=data4_unique_source.rename(columns={'coin':'No._of_entries_of_coin'})
 data4_unique_source.to_csv("data4_unique_coin.csv")
 data4['new_date'] = [d.date() for d in data4['date']] 
 data4['new_time'] = [d.time() for d in data4['date']]
 data4_unique_date =pd.DataFrame(data4.new_date.value_counts())
-data4_unique_date=data4_unique_date.rename(columns={'new_date':'No._of_unique_date'})
+data4_unique_date=data4_unique_date.rename(columns={'new_date':'No._of_entries_on_each_date'})
 data4_unique_date.to_csv("data4_unique_date.csv")
