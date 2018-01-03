fb41cbf2b8bf4a01051d6639e278fdbcf40e2f8c
PallabeeDey <pallabeedey@gmail.com>
2018-01-03 15:46:33 +0530

    first commit

--- ed_trade_data/readjson.py
+++ ed_trade_data/readjson.py
@@ -1,15 +1,17 @@
 import json
 import pandas as pd
 from pprint import pprint
+import numpy as np
 fp = open('/home/pallabeedey/Downloads/ed_trade_data.json') 
 
 data = json.load(fp)
 kys =data.keys()
-a=data['ETH_NTC']
-#pprint(data)
-#type(data)
+
 for i in range(0,275):
-    kys[i]=pd.DataFrame(data[kys[i]])
+    key=np.repeat([kys[i]],len(data[kys[i]]))
+    k=pd.DataFrame(data[kys[i]],index=key)
+    k['amount']=pd.to_numeric(k['amount'])
+    kys[i]=k[np.abs(k.amount-k.amount.mean())>(3*k.amount.std())]
 
 
 
